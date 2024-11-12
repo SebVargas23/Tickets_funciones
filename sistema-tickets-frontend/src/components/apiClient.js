29a6c70b0b502cc24394f30b9fd6db58257529ec
FelipeOjedaM <fe.ojeda@duocuc.cl>
2024-11-12 14:36:37 -0300

    arreglo de url momentaneo
    se corrige url de apiClient

--- sistema-tickets-frontend/src/components/apiClient.js
+++ sistema-tickets-frontend/src/components/apiClient.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',  // La URL base de tu API Django
+    baseURL: 'https://heron-eminent-starling.ngrok-free.app/',  // La URL base de tu API Django
     headers: {
-      //'ngrok-skip-browser-warning': 'any-value',
+      'ngrok-skip-browser-warning': 'any-value',
       'Content-Type': 'application/json',
       },
   });
